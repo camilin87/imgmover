2e43ac50aa1a5fdf42b2476a1d2241df848facb4
camilin87 <camilin87@gmail.com>
2015-02-18 19:05:15 -0500

    get the subdirectories

--- rakefile.rb
+++ rakefile.rb
@@ -5,10 +5,31 @@ def basedir
 end
 
 task :move_folder_debug do
-    puts "moving folders DEBUG"
-    puts basedir
+    move_folders_internal(true)
 end
 
 task :move_folders do
-    puts "moving folders"
+    move_folders_internal()
+end
+
+def move_folders_internal(is_debug=false)
+    environment = is_debug ? "DEBUG" : ""
+    puts "moving folders #{environment}"
+    puts basedir
+
+    for dirname in get_subdirs(basedir) do
+        if not should_ignore_directory(dirname)
+            puts dirname
+        end
+    end
+end
+
+def get_subdirs(parent_dir)
+    return Dir.entries(parent_dir).select { |entry|
+        File.directory? entry and !(entry =='.' || entry == '..') 
+    }
+end
+
+def should_ignore_directory(directory_name)
+    return directory_name.start_with?("_")
 end
