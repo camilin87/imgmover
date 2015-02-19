008d514fb490e09bbf79fbad86400da3682ac1f1
camilin87 <camilin87@gmail.com>
2015-02-18 22:11:45 -0500

    successfully extract the year of everyfolder

--- rakefile.rb
+++ rakefile.rb
@@ -22,14 +22,22 @@ def move_folders_internal(is_debug=false)
             full_dir_path = get_full_path dirname
 
             for filename in get_filenames full_dir_path do
-                # puts filename
+                if not should_ignore_file filename
+                    puts "Analyzing #{filename}"
 
-                std_filename = standardize_filename filename
-                puts std_filename
+                    exif_raw_data = read_exif filename
+                    year = extract_year exif_raw_data
 
-                # output = `exiftool #{filename}`
-                # puts output
+                    puts year
 
+                    if year.length > 0
+                        puts "moving folder to _#{year}"
+
+                        break
+                    end
+                else
+                    puts "Skipping #{filename}"
+                end
             end
 
             # puts full_dir_path
@@ -47,6 +55,15 @@ def should_ignore_directory(directory_name)
     return directory_name.start_with?("_")
 end
 
+def should_ignore_file(filename)
+    for ext in [".mov", ".png"] do
+        if filename.downcase.end_with? ext
+            return true
+        end
+    end
+    return false
+end
+
 def get_full_path(directory_name)
     return File.join(basedir, directory_name)
 end
@@ -60,4 +77,27 @@ def standardize_filename(full_path)
         full_path.gsub!("#{c}"){"\\#{c}"}
     end
     return full_path
+end
+
+def read_exif(filename)
+    std_filename = standardize_filename filename                    
+    return `exiftool #{std_filename}`
+end
+
+def extract_year(exif_raw_data)
+    original_time_arr = exif_raw_data.split(/(\n)/).delete_if{ |l|
+        not l.include? "Date/Time Original" 
+    }
+    # puts original_time_arr
+
+    if original_time_arr.size == 0
+        return ""
+    else
+        return extract_year_from_time_str original_time_arr[0]
+    end
+end
+
+def extract_year_from_time_str(exif_time_line)
+    # Date/Time Original              : 2013:09:25 19:36:11.598
+    return exif_time_line.split(/(:)/)[2].strip
 end
